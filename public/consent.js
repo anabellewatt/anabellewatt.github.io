37440a074f89e0d55883a7eae8ffbfce710d6253
bellewatt <157599059+anabellewatt@users.noreply.github.com>
2024-07-18 11:53:07 +0100

    Update consent.js
    fix the issue with server

--- public/consent.js
+++ public/consent.js
@@ -4,26 +4,10 @@ document.getElementById('consent-form').addEventListener('submit', function(e) {
 
     if (agreed) {
         // If user agrees, submit the form
-        fetch('/submit-consent', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ agreed: true })
-        })
-            .then(response => {
-                if (response.ok) {
-                    // Redirect to the survey page if agreement is given
-                    window.location.href = '/public/Survey.html';
-                } else {
-                    console.error('Failed to submit consent');
-                    alert('Failed to submit consent');
-                }
-            })
-            .catch(error => {
-                console.error('Error submitting consent:', error);
-                alert('Error submitting consent');
-            });
+        // Store consent in localStorage
+        localStorage.setItem('consentGiven', 'true');
+        // Redirect to the survey page
+        window.location.href = '/public/Survey.html';
     } else {
         // If user doesn't agree, show an alert
         alert('You must agree to participate in this survey to continue.');
